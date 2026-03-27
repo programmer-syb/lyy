39c39f7922bdffea2e5a669718e33707ce1bffe4
programmer-syb <2330419351@qq.com>
2026-03-27 15:38:08 +0800

    框架搭建

---
+++ src/main/java/org/example/lyy/LyyApplication.java
@@ -0,0 +1,13 @@
+package org.example.lyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lyy/LyyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lyy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
